e7a9d0a49ad471b1ed4f9f6fd37063521b6e7fbc
cjn <969921476@qq.com>
2020-07-03 14:31:42 +0800

    ïâ修改后的配置

--- web/src/main/java/com/cjn/controller/TestController.java
+++ web/src/main/java/com/cjn/controller/TestController.java
@@ -1,10 +1,18 @@
 package com.cjn.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author： cjn
  * @create： 2020-07-03 11:34
  * @Description：
  * @Modified By:
  */
+@Controller
 public class TestController {
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
 }
